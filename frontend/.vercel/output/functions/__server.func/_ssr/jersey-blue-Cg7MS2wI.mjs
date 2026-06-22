ddc05c927308377c53f6dda6061df09f190aa784
Pronoysaha90 <pronoysaha623@gmail.com>
2026-06-22 19:12:41 +0600

    Remove .vercel from tracking

--- frontend/.vercel/output/functions/__server.func/_ssr/jersey-blue-Cg7MS2wI.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/jersey-blue-Cg7MS2wI.js
-var jersey_blue_default = "/assets/jersey-blue-CGPVeF6U.jpg";
-//#endregion
-export { jersey_blue_default as t };
